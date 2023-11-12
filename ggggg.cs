1dedf3f99a598fc6413a45c30712af3e71be956e
ctepan707 <150595907+ctepan707@users.noreply.github.com>
2023-11-12 17:49:48 +0500

    Add files via upload

---
+++ ggggg.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ggggg : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int a = 3135346;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+}
